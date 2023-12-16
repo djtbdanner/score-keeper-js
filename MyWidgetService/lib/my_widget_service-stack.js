8667161a52807af6c87d66e06a3a62efb5ebf7cd
Dave Danner <djtbsdanner@yahoo.com>
2023-12-16 16:37:46 -0600

    initial fully working CDK deploys https/route/elb/container

--- MyWidgetService/lib/my_widget_service-stack.js
+++ MyWidgetService/lib/my_widget_service-stack.js
@@ -1,13 +1,21 @@
-const { Stack, App } = require('aws-cdk-lib');
-const { ContainerImage, FargateTaskDefinition, Ec2Service, FargateService } = require('aws-cdk-lib/aws-ecs');
-const { Vpc, SecurityGroup } = require('aws-cdk-lib/aws-ec2');
-const ecs = require('aws-cdk-lib/aws-ecs');
-const logs = require('aws-cdk-lib/aws-logs');
-const ec2 = require('aws-cdk-lib/aws-ec2');
-const { ApplicationLoadBalancer } = require('aws-cdk-lib/aws-elasticloadbalancingv2'); // Import ApplicationLoadBalancer
-const { Role } = require('aws-cdk-lib/aws-iam');
+
+const DOMAIN_NAME = 'davedanner.com';
+const CERTIFICATE_ARN = 'arn:aws:acm:us-east-1:068092817236:certificate/d95e229b-3e99-4d81-aa30-1a51fb233591';
+
+const { ApplicationLoadBalancer, ApplicationProtocol, ListenerAction, ListenerCertificate } = require('aws-cdk-lib/aws-elasticloadbalancingv2'); // Import ApplicationLoadBalancer
+const {
+  Cluster,
+  ContainerImage,
+  DeploymentControllerType,
+  FargatePlatformVersion,
+  FargateService,
+  FargateTaskDefinition,
+  LogDriver } = require('aws-cdk-lib/aws-ecs');
+const { HostedZone, ARecord, RecordTarget, Duration } = require('aws-cdk-lib/aws-route53');
 const { LogGroup, RetentionDays, DescribeLogGroupsCommand } = require('aws-cdk-lib/aws-logs');
-const { CfnService } = require('aws-cdk-lib/aws-ecs');
+const { Peer, Port, Vpc, SecurityGroup } = require('aws-cdk-lib/aws-ec2');
+const { RemovalPolicy, Stack } = require('aws-cdk-lib');
+const { Role } = require('aws-cdk-lib/aws-iam');
 
 class MyWidgetServiceStack extends Stack {
   constructor(scope, id, props) {
@@ -16,24 +24,25 @@ class MyWidgetServiceStack extends Stack {
     const defaultVpc = Vpc.fromLookup(this, 'DefaultVpc', { isDefault: true });
 
     // Create ECS Cluster in the default VPC
-    const cluster = new ecs.Cluster(this, `${props.env.appName}Cluster`, {
+    const cluster = new Cluster(this, `${props.env.appName}Cluster`, {
       vpc: defaultVpc,
     });
 
     // Retrieve the existing ECS task execution role
     const ecsTaskExecutionRole = Role.fromRoleArn(this, 'ExistingECSTaskExecutionRole', 'arn:aws:iam::068092817236:role/ecsTaskExecutionRole');
-    const sg = new ec2.SecurityGroup(this, `${props.env.appName}SecurityGroup`, {
+    const sg = new SecurityGroup(this, `${props.env.appName}SecurityGroup`, {
       vpc: defaultVpc,
-      description: `Security group for ECS tasks in ${props.env.appName}` 
+      description: `Security group for ECS tasks in ${props.env.appName}`
     });
-    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP in');
-    sg.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP out');
+    sg.addIngressRule(Peer.anyIpv4(), Port.tcp(80), 'Allow HTTP in');
+    sg.addEgressRule(Peer.anyIpv4(), Port.tcp(80), 'Allow HTTP out');
 
-    // **  cannot check if logs exist so need to comment this in out as needed - just delete the log group manually I guess
+    // log group
     const theName = `ecs/${props.env.appName}LogGroup`;
     const logGroup = new LogGroup(this, `${props.env.appName}LogGroup`, {
       logGroupName: theName,
       retention: RetentionDays.ONE_MONTH, // Adjust retention as needed
+      removalPolicy: RemovalPolicy.DESTROY,// this allows the log group to be destroyed when destroy runs
     });
 
     // Create ECS Task Definition for Fargate
@@ -43,31 +52,28 @@ class MyWidgetServiceStack extends Stack {
     });
 
     taskDefinition.addContainer(`${props.env.appName}Container`, {
-      image: ecs.ContainerImage.fromRegistry(`${props.env.account}.dkr.ecr.${props.env.region}.amazonaws.com/${props.env.appName}:latest`),
+      image: ContainerImage.fromRegistry(`${props.env.account}.dkr.ecr.${props.env.region}.amazonaws.com/${props.env.appName}:latest`),
       portMappings: [{ containerPort: 80 }],
       memoryLimitMiB: 512,
       cpu: 256,
       essential: true,
-      logging: ecs.LogDriver.awsLogs({
+      logging: LogDriver.awsLogs({
         logGroup,
         streamPrefix: `${props.env.appName}Container`,
       }),
     });
 
-    console.log("starting the service");
-    const securityGroupIds = this.node.tryGetContext('SecurityGroupIDs') || ['sg-033785b39f4978a4b'];
-
     // ECS Service
-    const ecsService = new ecs.FargateService(this, `${props.env.appName}Service`, {
+    const ecsService = new FargateService(this, `${props.env.appName}Service`, {
       cluster,
       serviceName: `${props.env.appName}Service`,
       taskDefinition,
       executionRole: ecsTaskExecutionRole,
       assignPublicIp: true,
       desiredCount: 1,
-      platformVersion: ecs.FargatePlatformVersion.LATEST, // Update with your desired version
+      platformVersion: FargatePlatformVersion.LATEST, 
       deploymentController: {
-        type: ecs.DeploymentControllerType.ECS,
+        type: DeploymentControllerType.ECS,
       },
       securityGroups: [sg],
       maximumPercent: 100,
@@ -76,67 +82,64 @@ class MyWidgetServiceStack extends Stack {
         enable: true,
         rollback: true,
       },
-      logging: ecs.LogDriver.awsLogs({
+      logging: LogDriver.awsLogs({
         logGroup,
         streamPrefix: `${props.env.appName}Service`,
       }),
-      
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // Application Load Balancer
+    const loadBalancer = new ApplicationLoadBalancer(this, `${props.env.appName}LoadBalancer`, {
+      vpc: defaultVpc,
+      internetFacing: true,
+    });
+
+    // Create HTTP listener for redirection
+    const httpListener = loadBalancer.addListener(`${props.env.appName}HttpListener`, {
+      port: 80,
+      protocol: ApplicationProtocol.HTTP,
+      defaultAction: ListenerAction.redirect({
+        protocol: ApplicationProtocol.HTTPS,
+        port: '443',
+        permanent: true, // Set to 'false' if you want a temporary redirect
+      }),
+    });
+
+    // Create https listener
+    const httpsListener = loadBalancer.addListener(`${props.env.appName}HttpsListener`, {
+      port: 443,
+      protocol: ApplicationProtocol.HTTPS,
+      defaultAction: ListenerAction.redirect({
+        protocol: ApplicationProtocol.HTTPS,
+        port: '443',
+      }),
+      certificates: [ListenerCertificate.fromArn(CERTIFICATE_ARN)],
+    });
+
+    // Add Fargate service as a target to the HTTPS listener
+    httpsListener.addTargets(`${props.env.appName}HttpsTarget`, {
+      targets: [ecsService],
+      port: 80, // Fargate service is listening on port 80
+    });
+
+    // Route 53 hosted zone
+    const hostedZone = HostedZone.fromLookup(this, `${props.env.appName}HttpsTarget`, {
+      domainName: DOMAIN_NAME, // domain name
+    });
+
+    // Create A record in Route 53
+    new ARecord(this, `${props.env.appName}DNSRecord`, {
+      zone: hostedZone,
+      target: RecordTarget.fromAlias({
+        bind: () => ({
+          dnsName: loadBalancer.loadBalancerDnsName,
+          hostedZoneId: loadBalancer.loadBalancerCanonicalHostedZoneId,
+        }),
+      }),
+      recordName: 'score', // desired subdomain
     });
   }
 }
 
-// // Application Load Balancer
-// const loadBalancer = new ApplicationLoadBalancer(this, `${props.env.appName}LoadBalancer`, {
-//   vpc: defaultVpc,
-//   internetFacing: true,
-// });
-
-// // Listener for HTTP (80)
-// const httpListener = loadBalancer.addListener('HttpListener', {
-//   port: 80,
-//   protocol: ApplicationProtocol.HTTP,
-// });
-
-// // Redirect HTTP to HTTPS
-// httpListener.addAction('HttpRedirectAction', {
-//   action: {
-//     type: 'redirect',
-//     redirectAction: {
-//       protocol: 'HTTPS',
-//       port: '443',
-//     },
-//   },
-// });
-
-// // Listener for HTTPS (443)
-// const httpsListener = loadBalancer.addListener('HttpsListener', {
-//   port: 443,
-//   protocol: ApplicationProtocol.HTTPS,
-//   certificateArns: ['arn:aws:acm:us-east-1:068092817236:certificate/d95e229b-3e99-4d81-aa30-1a51fb233591'], // Replace with your SSL certificate ARN
-// });
-
-// // Attach ECS service to ALB
-// httpsListener.addTargets('EcsTarget', {
-//   targets: [service],
-//   healthCheck: {
-//     path: '/',
-//     interval: Duration.seconds(30000),
-//   },
-// });
-
-// // Route 53 DNS record
-// const hostedZone = HostedZone.fromLookup(this, 'davedanner.com', {
-//   domainName: 'score.davedanner.com',
-// });
-
-// new ARecord(this, 'YourDnsRecord', {
-//   zone: hostedZone,
-//   target: RecordTarget.fromAlias({ bind: () => loadBalancer.loadBalancerDnsName }),
-//   recordName: 'your-subdomain', // You can set the subdomain here
-//   ttl: Duration.minutes(5),
-// });
-// }
-// }
-
 module.exports = { MyWidgetServiceStack }
-
